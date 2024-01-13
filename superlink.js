bdde2ccc7ef19e878e36815e738fd79048d6705e
尚寂新 <shangjixin@outlook.com>
2024-01-13 13:13:32 +0800

    使用原生JS重写，移除jQuery依赖

--- superlink.js
+++ superlink.js
@@ -1,35 +1,39 @@
-$(".JixinParser-card .JixinParser-card-meta span.fold").click(function(){
-    if ($(this).parent().next().children().length > 0 ) {
-        $(this).parent().next().empty();
-    } else {
-        $(this).parent().next().append('<iframe src="'+ $(this).parent().parent().attr('data-src') +'" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true"> </iframe>');
-    }
-});
+window.onload = function() {
 
-window.onload = function(){
-    $(".JixinParser-card.github .iframe-container").attr("style","padding: 3px 10px;font-size: 13px;");
-    var all = document.querySelectorAll('.JixinParser-card.github');
-    all.forEach(function(ele){
-        $.ajax({
-            url: ele.getAttribute('data-src'),
-            type: "get",
-            success: function(data){
-            
-                //console.log(data);
-                ele.children[1].innerHTML = `${data.description != null ? data.description : ""}<br>
-                                            ${data.homepage != null ? data.homepage : ""}<br><br>
-                                            <span style="color:#666">Star: ${data.stargazers_count}</span>&nbsp;&nbsp;
-                                            <span style="color:#666">Fork: ${data.forks_count}</span>&nbsp;&nbsp;
-                                            <span style="color:#666">Lang: ${data.language}</span>&nbsp;&nbsp;
-                                            <span style="color:#666">Branch: ${data.default_branch}</span>&nbsp;&nbsp;
-                                            `;
-    
-            },
-            error: function(data){
-                ele.children[1].append(data.responseJSON.message ? data.responseJSON.message : "Ajax Request Failed!");
-                // console.log(data);
+    document.querySelectorAll(".JixinParser-card .JixinParser-card-meta span.fold").forEach(function(element) {
+        element.addEventListener("click", function() {
+            var parentNext = this.parentElement.nextElementSibling;
+            if (parentNext.children.length > 0) {
+                parentNext.innerHTML = '';
+            } else {
+                parentNext.innerHTML = '<iframe src="'+ this.parentElement.parentElement.getAttribute('data-src') +'" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true"> </iframe>';
             }
         });
-
     });
-}
+
+    document.querySelectorAll('.JixinParser-card.github .iframe-container').forEach(function(container) {
+    
+        var url = container.parentElement.getAttribute('data-src');
+    
+        fetch(url)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                }
+                return response.json();
+            })
+            .then(data => {
+                container.innerHTML = `${data.description != null ? data.description : ""}<br>
+                                                ${data.homepage != null ? data.homepage : ""}<br><br>
+                                                <span style="color:#666">Star: ${data.stargazers_count}</span>&nbsp;&nbsp;
+                                                <span style="color:#666">Fork: ${data.forks_count}</span>&nbsp;&nbsp;
+                                                <span style="color:#666">Lang: ${data.language}</span>&nbsp;&nbsp;
+                                                <span style="color:#666">Branch: ${data.default_branch}</span>&nbsp;&nbsp;
+                                                `;
+            })
+            .catch(error => {
+                container.innerHTML = error.message ? error.message : "Fetch Request Failed!";
+            });
+    });
+
+};
